d32791850a5808c668523d2d1f7ef0c3cb088c0a
Petrushchenko <petrushchenko.l@ukr.net>
2019-07-02 16:42:11 +0300

    Updates

---
+++ precache-manifest.7b79ea754c298b94b07649f1c18f929c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "671115908ab4714391ff0299caf0a0d3",
+    "url": "/game/index.html"
+  },
+  {
+    "revision": "d1e204055b4daf86d4a7",
+    "url": "/game/static/css/main.f3c44789.chunk.css"
+  },
+  {
+    "revision": "fd9e158a2069902e9b43",
+    "url": "/game/static/js/2.809d5a0e.chunk.js"
+  },
+  {
+    "revision": "d1e204055b4daf86d4a7",
+    "url": "/game/static/js/main.555708cd.chunk.js"
+  },
+  {
+    "revision": "bab8f84a0f4d6b7ca58b",
+    "url": "/game/static/js/runtime~main.2d0fbc73.js"
+  },
+  {
+    "revision": "02118ba99f427cf3d0bacb61f0b67fdd",
+    "url": "/game/static/media/down-arrow.02118ba9.svg"
+  },
+  {
+    "revision": "b607fb43c5c4b873bd826f152cfdb9ba",
+    "url": "/game/static/media/left-arrow.b607fb43.svg"
+  },
+  {
+    "revision": "302ee0697048983065756ccd94841ba2",
+    "url": "/game/static/media/like.302ee069.svg"
+  },
+  {
+    "revision": "9892d59c860902d50f78fb6c50181680",
+    "url": "/game/static/media/right-arrow.9892d59c.svg"
+  },
+  {
+    "revision": "cf99f7d3d46ab350eab5dea50d66fa96",
+    "url": "/game/static/media/thumb-down.cf99f7d3.svg"
+  },
+  {
+    "revision": "daa5091a9c9d808196ee75a6ac9ac9a7",
+    "url": "/game/static/media/up-arrow.daa5091a.svg"
+  }
+]);
